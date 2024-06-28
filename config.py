94045604df875f667ec6110eb9bb8af229340630
Ananya <117035260+ananya15082002@users.noreply.github.com>
2024-06-28 16:30:01 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # Example SQLite URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Optional, but recommended for performance
-
-    SECRET_KEY = 'your_secret_key_here'  # Replace with a secure secret key
-    GOOGLE_OAUTH_CLIENT_ID =''#replace with your
-    GOOGLE_OAUTH_CLIENT_SECRET = ''#replace with your
